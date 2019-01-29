793c3debfa45140b8a93acf591dcbd36c2c53504
deepak rana <34414032+daring-891@users.noreply.github.com>
2019-01-29 16:40:02 +0530

    Add files via upload

---
+++ otpgenrator.ino
@@ -0,0 +1,22 @@
+#include<String.h>
+int w;
+String otp;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    otp="";
+    int i=0; 
+    for(i=0;i<6;i++)
+    {
+      w=random(10);
+      otp+=w;
+    }
+    Serial.println("otp form is " + otp);
+  delay(1000);
+
+}
